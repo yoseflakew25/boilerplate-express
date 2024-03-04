d5cffe95d8be1f289d2350eb0954510835a8356b
yosef lakew <JOLAROHE@GMAIL.COM>
2024-03-04 23:08:37 +0000

    .

--- myApp.js
+++ myApp.js
@@ -2,6 +2,10 @@ let express = require('express');
 let app = express();
 let path = require('path');
 
+// Mount the express.static() middleware
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Route handler for the root path
 app.get('/', function(req, res) {
   let filePath = path.join(__dirname, 'views', 'index.html');
   res.sendFile(filePath);
