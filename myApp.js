193dc301373400d722b015ff0e6ccbf51496ab91
yosef lakew <JOLAROHE@GMAIL.COM>
2024-03-05 08:06:56 +0000

    .

--- myApp.js
+++ myApp.js
@@ -1,28 +1,23 @@
 require('dotenv').config();
-let express = require('express');
-let app = express();
-let path = require('path');
+
+const express = require('express');
+const app = express();
+const path = require('path');
 
 // Mount the express.static() middleware
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
-app.get('/json', function(req, res) {
-    let message = 'Hello json';
-  
-    // Check the value of the MESSAGE_STYLE environment variable
-    if (process.env.MESSAGE_STYLE === "allCaps") {
-        message = "Hello World".toUpperCase();
-      } else {
-        message = "Hello World";
-      }
-  
-    let json = {
-      message: message
-    };
-  
-    res.json(json);
-  });
+app.get('/json', (req, res) => {
+  let message = 'Hello json';
+
+  // Check the value of the MESSAGE_STYLE environment variable
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message });
+});
 
 // Route handler for the root path
 app.get('/', function(req, res) {
