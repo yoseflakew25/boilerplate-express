c2b853e988280609af4f3bdf7ec9b4cb34d7a2e9
yosef lakew <JOLAROHE@GMAIL.COM>
2024-03-05 11:30:06 +0000

    .

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv-flow').config();
 
 const express = require('express');
 const app = express();
@@ -10,10 +10,9 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/json', (req, res) => {
   let message = 'Hello json';
-  let MESSAGE_STYLE='uppercase'
 
   // Check the value of the MESSAGE_STYLE environment variable
-  if (MESSAGE_STYLE === 'uppercase') {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
 
@@ -21,9 +20,9 @@ app.get('/json', (req, res) => {
 });
 
 // Route handler for the root path
-app.get('/', function(req, res) {
-  let filePath = path.join(__dirname, 'views', 'index.html');
-  res.sendFile(filePath);
-});
+// app.get('/', function(req, res) {
+//   let filePath = path.join(__dirname, 'views', 'index.html');
+//   res.sendFile(filePath);
+// });
 
 module.exports = app;
